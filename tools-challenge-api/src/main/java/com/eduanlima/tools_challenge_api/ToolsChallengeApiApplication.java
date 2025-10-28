00c8ef66d921e2ba1be9e691c436a9e53953a0c1
eduanlima@github.com <eduandev@gmail.com>
2025-10-28 20:55:20 -0300

    Projeto iniciado

---
+++ tools-challenge-api/src/main/java/com/eduanlima/tools_challenge_api/ToolsChallengeApiApplication.java
@@ -0,0 +1,13 @@
+package com.eduanlima.tools_challenge_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsChallengeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolsChallengeApiApplication.class, args);
+	}
+
+}
---
+++ tools-challenge-api/src/test/java/com/eduanlima/tools_challenge_api/ToolsChallengeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduanlima.tools_challenge_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsChallengeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
